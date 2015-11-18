096d4a814924a9d6f8790e5eca3b18bcd2cee4b5
DontKnowAnything <pcmcknew1@mymail.aacc.edu>
2015-11-18 16:22:57 -0500

    Create PortfolioJS.js

---
+++ PortfolioJS.js
@@ -0,0 +1,22 @@
+(function (){
+//function draw(){
+var hC=document.getElementById("c");
+        var con=hC.getContext("2d");
+    z();
+
+function z(){
+    window.addEventListener('resize',size(),false);
+        size();
+}
+
+        function re(){
+            con.strokeStyle='blue';
+            con.lineWidth="5";
+            con.strokeRect(0,0,window.innerWidth,window.innerHeight);
+        }
+        function size(){
+hC.width=window.innerWidth;
+re();
+}
+//}
+})();
